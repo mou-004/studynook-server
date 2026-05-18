475558b67ec3bee6463655c6caf63cd452949c44
Halima Akther <halimaakthermou004@gmail.com>
2026-05-19 01:02:02 +0600

    Update CORS origin to allow multiple URLs

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://studynook-09.netlify.app",
+    origin: [
+      "http://localhost:5175",
+      "https://studynook-09.netlify.app",
+    ],
     credentials: true,
   })
 );
