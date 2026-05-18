a3635324617c930de5a4ad05497e3a8e307d2cce
Halima Akther <halimaakthermou004@gmail.com>
2026-05-19 00:33:27 +0600

    Update CORS origin to production URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5175",
+    origin: process.env.CLIENT_URL || "https://studynook-09.netlify.app",
     credentials: true,
   })
 );
