767f467d2c90ecd2c7c3d0a116a8e8be8bf36a6f
Halima Akther <halimaakthermou004@gmail.com>
2026-05-19 15:11:49 +0600

    Enhance auth middleware to support token from headers

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 
 export default function auth(req, res, next) {
   try {
-    const token = req.cookies?.token;
+    const token =
+      req.cookies?.token ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
@@ -15,7 +17,7 @@ export default function auth(req, res, next) {
     };
 
     next();
-  } catch {
+  } catch (error) {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 }
